7fbd3221a072fc1ea408f26c75e53220e14f2493
Saad Khalid <saadkhalid913@gmail.com>
2022-01-29 22:51:08 -0500

    tshirt test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useRef, useState } from "react"
+import shirt from "./shirt.jpeg"
 function App() {
+    const canvasRef = useRef()
+
+    const [nft, setNFT] = useState("")
+
+    const NFT = new Image()    
+
+    const img = new Image()
+    img.src = shirt
+    NFT.src = nft
+
+
+    img.onload = function(){
+        if (canvasRef.current) {
+          const ctx = canvasRef.current.getContext("2d")
+          ctx.drawImage(img,0,0); // Or at whatever offset you like
+        }
+    };
+
+    NFT.onload = function() {
+      if (canvasRef.current) {
+        const ctx = canvasRef.current.getContext("2d")
+        ctx.drawImage(NFT,185,200, 125,125); // Or at whatever offset you like
+      }
+    }
+
+    const download = () => {
+      const image = canvasRef.current.toDataURL("image/png")
+      var link = document.createElement('a');
+      link.download = "my-image.png";
+      link.href = image;
+      link.click();
+  }
+  const setter = (e) => {
+    const image = e.target.files[0]
+    setNFT(URL.createObjectURL(image))
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d")
+      ctx.clearRect(0,0,500,600)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input onChange={setter} type = "file" accept="image/*" />
+        <button onClick={download}>Create Thingy</button>
+        <canvas ref = {canvasRef} style={{border: "1px solid black"}} width="500" height="600"></canvas>
     </div>
   );
 }
